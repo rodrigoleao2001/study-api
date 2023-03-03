e53fcef3e03cc97550276b12abce10272750e949
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-02 21:40:10 -0300

    commit inicial

---
+++ src/main/java/com/github/rodrigosuhaileao/study_api/Application.java
@@ -0,0 +1,30 @@
+package com.github.rodrigosuhaileao.study_api;
+
+
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+	private static Logger LOG = 
+			LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("Estou testando o log");
+		
+	}
+
+
+}
---
+++ src/test/java/com/github/rodrigosuhaileao/study_api/StudyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rodrigosuhaileao.study_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
